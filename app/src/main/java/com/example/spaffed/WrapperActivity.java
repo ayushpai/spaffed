82667807bb9c427d3263577a97f39cb275c1c788
Alex Kranias <alexander.kranias@gmail.com>
2024-04-12 11:20:06 -0400

    view flipper functionality

--- app/src/main/java/com/example/spaffed/WrapperActivity.java
+++ app/src/main/java/com/example/spaffed/WrapperActivity.java
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ViewFlipper;
 
 public class WrapperActivity extends AppCompatActivity {
+
+    private ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wrapper);
+
+        viewFlipper = findViewById(R.id.wrappedFlipper);
     }
 
     public void previousView(View view) {
+        viewFlipper.showPrevious();
     }
 
     public void nextView(View view) {
+        viewFlipper.showNext();
     }
 }
