a784df4628928dbef5d976ba7c04a45591253586
Ayush Pai <ayushpai@gmail.com>
2024-04-12 08:20:42 -0400

    Update SpotifyLoginActivity.java

--- app/src/main/java/com/example/spaffed/SpotifyLoginActivity.java
+++ app/src/main/java/com/example/spaffed/SpotifyLoginActivity.java
@@ -10,6 +10,6 @@ public class SpotifyLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spotify_login);
-        
+
     }
 }
