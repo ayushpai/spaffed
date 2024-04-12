3f5ba9c6123551a4867fc041d709c09509651c02
Alex Kranias <alexander.kranias@gmail.com>
2024-04-12 08:48:55 -0400

    new WrapperActivity

---
+++ app/src/main/java/com/example/spaffed/WrapperActivity.java
@@ -0,0 +1,14 @@
+package com.example.spaffed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WrapperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrapper);
+    }
+}
