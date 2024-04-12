e03d00baab7b0bea39d4fe87307f02e1eb376181
Ayush Pai <ayushpai@gmail.com>
2024-04-12 08:18:56 -0400

    added spotify login page

---
+++ app/src/main/java/com/example/spaffed/SpotifyLoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.spaffed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpotifyLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spotify_login);
+        
+    }
+}
