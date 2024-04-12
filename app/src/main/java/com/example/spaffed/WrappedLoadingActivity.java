92c2cb75346c504577ccd52dca28676344e1f719
Alex Kranias <alexander.kranias@gmail.com>
2024-04-12 11:14:09 -0400

    colors

--- app/src/main/java/com/example/spaffed/WrappedLoadingActivity.java
+++ app/src/main/java/com/example/spaffed/WrappedLoadingActivity.java
@@ -3,19 +3,12 @@ package com.example.spaffed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class WrapperActivity extends AppCompatActivity {
+public class WrappedLoadingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wrapper);
-    }
-
-    public void previousView(View view) {
-    }
-
-    public void nextView(View view) {
+        setContentView(R.layout.activity_wrapped_loading);
     }
 }
